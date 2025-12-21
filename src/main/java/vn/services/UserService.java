331b32ce9de5de10ceb3d97728acb83e0dbb75e5
NgocAnh998 <23130017@st.hcmuaf.edu.vn>
2025-12-21 19:22:12 +0700

    Chỉnh product card

--- src/main/java/vn/services/UserService.java
+++ src/main/java/vn/services/UserService.java
@@ -28,6 +28,6 @@ public class UserService {
         User user = getUserByUserName(username);
         if(user == null)return false;
         if(user.getPassword().equals(password)) return true;
-        return false;
+        return false;   
     }
 }
