2f0ba3a6aaf41aecdd0d2dde909c5e0128d2b299
Hungthinh17032002nlu <20130123@st.hcmuaf.edu.vn>
2026-01-04 18:48:03 +0700

    Fix Order model and OrderService

--- src/main/java/vn/services/OrderService.java
+++ src/main/java/vn/services/OrderService.java
@@ -1,15 +1,20 @@
 package vn.services;
 
-import vn.dao.OrderDao;
 import vn.model.Order;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public class OrderService {
-//    OrderDao orderDao = new OrderDao();
-//    public List<Order> getListOrderById(int id){
-//        return orderDao.getListOrderById(id);
-//    }
 
+    public List<Order> getOrdersByUser(int userId) {
 
+        List<Order> list = new ArrayList<>();
+
+        list.add(new Order(1001, userId, "DELIVERED", "14/10/2025", 12480000));
+        list.add(new Order(1002, userId, "SHIPPING", "20/10/2025", 6490000));
+        list.add(new Order(1003, userId, "CANCELED", "22/10/2025", 2000000));
+
+        return list;
+    }
 }
